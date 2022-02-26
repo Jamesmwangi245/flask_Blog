7302ab4b45987aaed8d6470b477c257300031013
jamesmwangi254 <james.kariuki@student.moringaschool.com>
2022-02-26 19:54:08 +0300

    class

---
+++ models/articles.py
@@ -0,0 +1,17 @@
+class Articles:
+      '''
+  class to define articles object
+  '''
+def __init__(self,urlToImage,title,description,url,time,content):
+    self.urlToImage=urlToImage
+    self.title=title
+    self.description=description
+    self.url=url
+    self.time=time
+    self.content=content
+
+class Sources:
+  def __init__(self,id,name,url):
+    self.id=id
+    self.name=name
+    self.url=url
