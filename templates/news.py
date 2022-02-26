fdd2df098d355edbea0dc2d8082c2a46ff91779b
jamesmwangi254 <james.kariuki@student.moringaschool.com>
2022-02-26 21:57:55 +0300

    news

---
+++ templates/news.py
@@ -0,0 +1,16 @@
+# from os import name
+from flask import render_template
+from app import app
+from .request import get_articles, get_source
+
+@app.route('/')
+def index():
+  '''
+  Root page function that returns index page and its data
+  '''
+  # breakpoint()
+  sourcez=get_source('sources')
+  #get news article
+  fav_source=get_articles('sources')
+  title='News_Blog'
+  return render_template('index.html',title = title,sources=fav_source,source=sourcez)
