eb1fb670d9b7d095e51e11ec98efa2d0c007afaf
jamesmwangi254 <james.kariuki@student.moringaschool.com>
2022-02-26 21:50:41 +0300

    __init__

---
+++ models/__init__.py
@@ -0,0 +1,16 @@
+from ensurepip import bootstrap
+from flask import Flask
+from .config import DevConfig
+from flask_bootstrap import Bootstrap
+
+#initializing application
+app=Flask(__name__)
+
+#setting up configuration
+app.config.from_object(DevConfig)
+app.config.from_pyfile('config.py')
+
+#initializing Flask extension
+bootstrap = Bootstrap(app)
+
+from app import news
